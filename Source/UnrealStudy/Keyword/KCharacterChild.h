de1bb37b5f7898ed54c2f6602d75a6a5d89ac0b8
Mincheon Bae(배민천) <baemincheon@bluehole.net>
2019-07-17 14:24:56 +0900

    UCLASS : Abstract

---
+++ Source/UnrealStudy/Keyword/KCharacterChild.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Keyword/KCharacterParent.h"
+#include "KCharacterChild.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSTUDY_API AKCharacterChild : public AKCharacterParent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/UnrealStudy/Keyword/KGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSTUDY_API AKGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AKGameMode();
+	
+};
