0dc4d9a76c85f27750e1326fa5e07eb4302ceb70
baemincheon <baemincheon@gmail.com>
2019-07-26 13:23:54 +0900

    Replication : re-create sandbox

--- Source/UnrealStudy/Replication/RCharacter.cpp
+++ Source/UnrealStudy/Replication/RCharacter.cpp
@@ -1,21 +1,37 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "RCharacter.h"
-#include <Components/InputComponent.h>
+#include "UnrealStudy.h"
+#include "Components/InputComponent.h"
 
 ARCharacter::ARCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	Speed = 0.0f;
 }
 
 void ARCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		UE_LOG(LogServer, Warning, TEXT("Character::BeginPlay() by %s"), *GetName());
+	}
+	else
+	{
+		UE_LOG(LogClient, Warning, TEXT("Character::BeginPlay() by %s"), *GetName());
+	}
 }
 
 void ARCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	FVector Location = GetActorLocation();
+	Location += FVector(Speed * DeltaTime, 0.0f, 0.0f);
+	SetActorLocation(Location);
 }
 
 void ARCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -28,11 +44,33 @@ void ARCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 
 void ARCharacter::SpeedUp()
 {
-	;
+	if (HasAuthority())
+	{
+		UE_LOG(LogServer, Warning, TEXT("speed is %f in server"), Speed);
+
+		Speed += 1.0f;
+	}
+	else
+	{
+		UE_LOG(LogClient, Warning, TEXT("speed is %f in client"), Speed);
+	}
 }
 
 void ARCharacter::SpeedDown()
 {
-	;
+	if (HasAuthority())
+	{
+		UE_LOG(LogServer, Warning, TEXT("speed is %f in server"), Speed);
+
+		Speed -= 1.0f;
+		if (Speed < 0.0f)
+		{
+			Speed = 0.0f;
+		}
+	}
+	else
+	{
+		UE_LOG(LogClient, Warning, TEXT("speed is %f in client"), Speed);
+	}
 }
 
--- Source/UnrealStudy/Replication/RCharacter.h
+++ Source/UnrealStudy/Replication/RCharacter.h
@@ -24,4 +24,6 @@ public:
 private:
 	void SpeedUp();
 	void SpeedDown();
+
+	float Speed;
 };
